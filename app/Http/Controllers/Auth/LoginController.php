63af786b662f1c9b051e9936a6333cf4de2e527c
Yefry Moraga <jeffamed@gmail.com>
2021-09-10 17:44:20 -0600

    cambiando redirectTo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function redirectTo()
+    {
+        if (auth()->user()->is_admin || auth()->user()->user_type) {
+            return '/dashboard';
+        } else {
+            return '/';
+        }
+    }
 
     /**
      * Create a new controller instance.
